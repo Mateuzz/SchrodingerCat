0aaefc389d90591008c4cc41ffbfae7f17ae456e
Mateuzz <38082919+Mateus-F@users.noreply.github.com>
2021-11-17 18:24:31 -0300

    Update Player.gd

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,5 +1,6 @@
 extends Actor
 
+# Processa a velocidade que o player vai após pressionar um botão, e também a gravidade
 func _physics_process(delta: float) -> void:
 	var jump_was_interrupted = Input.is_action_just_released("move_jump") and velocity.y < 0
 	var direction = get_direction()
@@ -12,6 +13,7 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 	
+# Pega a direção que o Player ta indo baseado no input
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - 
@@ -19,6 +21,7 @@ func get_direction() -> Vector2:
 		-1.0 if Input.is_action_just_pressed("move_jump") and is_on_floor() else 1.0
 	)
 
+# Calcula a velocidade final do player
 func calculate_velocity(direction: Vector2, jump_was_interruted: bool) -> Vector2:
 	var new_velocity = velocity
 	
