0b1851e4d1fab9c30cf07af48beb957fd2c63e4f
Mateuzz <38082919+Mateus-F@users.noreply.github.com>
2021-11-17 18:27:11 -0300

    Update ChangeSceneButton.gd

--- src/InterfaceObjects/ChangeSceneButton.gd
+++ src/InterfaceObjects/ChangeSceneButton.gd
@@ -3,10 +3,12 @@ tool
 
 export(String, FILE) var scene := ""
 
+# Mensagem de erro no editor se não colocar a cena que o botão vai levar
 func _get_configuration_warning() -> String:
 	if scene == "":
 		return "ChangeSceneButton must have scene attached"
 	return ""
 
+# Muda a cena quando o botão é soltado
 func _on_ChangeSceneButton_button_up() -> void:
 	get_tree().change_scene(scene)
